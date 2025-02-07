c0de8c87b2f78a266f0b054cddb989a3a9f13c9e
Mateus Reis <mateuskonige@gmail.com>
2025-02-07 17:22:28 -0300

    make log user activity middleware

---
+++ app/Http/Middleware/LogUserActivity.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\AuditLog;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class LogUserActivity
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if (Auth::check()) {
+            AuditLog::create([
+                'user_id' => Auth::id(),
+                'action' => $request->method(),
+                'details' => json_encode($request->all()),
+            ]);
+        }
+
+        return $next($request);
+    }
+}

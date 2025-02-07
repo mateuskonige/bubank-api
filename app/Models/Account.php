03db7757176990e7d726f8930257ce5f883dcbc1
Mateus Reis <mateuskonige@gmail.com>
2025-02-07 11:39:26 -0300

    Account model

---
+++ app/Models/Account.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Account extends Model
+{
+    /** @use HasFactory<\Database\Factories\AccountFactory> */
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'balance',
+    ];
+
+    protected $casts = [
+        'balance' => 'integer'
+    ];
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AccountController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,4 +10,8 @@ Route::prefix('auth')->group(function () {
     Route::post('refresh', [AuthController::class, 'refresh']);
     Route::get('user', [AuthController::class, 'me']);
     Route::post('change-password', [AuthController::class, 'change_password']);
+});
+
+Route::middleware('auth')->group(function () {
+    Route::apiResource('accounts', AccountController::class);
 });

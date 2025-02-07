56eaa7f402a37a98ad36199ce2d7be1f1521328b
Mateus Reis <mateuskonige@gmail.com>
2025-02-07 17:20:05 -0300

    add transaction factory and audit channel

--- config/logging.php
+++ config/logging.php
@@ -51,6 +51,11 @@ return [
     */
 
     'channels' => [
+        'audit' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/audit.log'),
+            'level' => 'info',
+        ],
 
         'stack' => [
             'driver' => 'stack',
@@ -89,7 +94,7 @@ return [
             'handler_with' => [
                 'host' => env('PAPERTRAIL_URL'),
                 'port' => env('PAPERTRAIL_PORT'),
-                'connectionString' => 'tls://'.env('PAPERTRAIL_URL').':'.env('PAPERTRAIL_PORT'),
+                'connectionString' => 'tls://' . env('PAPERTRAIL_URL') . ':' . env('PAPERTRAIL_PORT'),
             ],
             'processors' => [PsrLogMessageProcessor::class],
         ],

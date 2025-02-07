c8c24894590aa1f8498cdd20aaab216cfcd53af1
Mateus Reis <mateuskonige@gmail.com>
2025-02-07 17:23:02 -0300

    make requests for models

---
+++ app/Http/Requests/StoreTransactionRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\TransactionType;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreTransactionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            "type" => ["required"],
+            "amount" => 'required|min:1',
+            "destination_account_id" => "nullable|exists:accounts,id",
+        ];
+    }
+}

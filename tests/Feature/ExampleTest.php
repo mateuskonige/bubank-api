02e79c01c5edc7c354f5f793541843297d390dc2
Mateus Reis <mateuskonige@gmail.com>
2025-02-13 16:55:47 -0300

    write some feature tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -3,6 +3,8 @@
 namespace Tests\Feature;
 
 // use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
@@ -15,5 +17,74 @@ class ExampleTest extends TestCase
         $response = $this->get('api/');
 
         $response->assertStatus(200);
+    }
+
+    public function test_user_can_soft_delete_their_account()
+    {
+        //arrange
+        $user = User::factory()->create();
+        $account = $user->account()->create([
+            'name' => 'Test Account',
+            'balance' => 0,
+        ]);
+
+        //act
+        $response = $this->actingAs($user)->delete('api/accounts/' . $account->id);
+
+        //assert
+        $response->assertStatus(204);
+        $this->assertSoftDeleted('accounts', ['id' => $account->id]);
+    }
+
+    public function test_user_can_not_soft_delete_their_account_if_balance_is_not_zero()
+    {
+        //arrange
+        $user = User::factory()->create();
+        $account = $user->account()->create([
+            'name' => 'Test Account',
+            'balance' => 1000,
+        ]);
+
+        //act
+        $response = $this->actingAs($user)->delete('api/accounts/' . $account->id);
+
+        //assert
+        $response->assertStatus(400);
+        $this->assertNotSoftDeleted('accounts', ['id' => $account->id]);
+    }
+
+    public function test_user_can_not_soft_delete_anothers_account()
+    {
+        //arrange
+        $user = User::factory()->create();
+        $account = User::factory()->create()->account()->create([
+            'name' => 'Test Account',
+            'balance' => 0,
+        ]);
+
+        //act
+        $response = $this->actingAs($user)->delete('api/accounts/' . $account->id);
+
+        //assert
+        $response->assertStatus(403);
+        $this->assertNotSoftDeleted('accounts', ['id' => $account->id]);
+    }
+
+    public function test_user_can_restore_their_account()
+    {
+        //arrange
+        $user = User::factory()->create();
+        $account = $user->account()->create([
+            'name' => 'Test Account',
+            'balance' => 0,
+            'deleted_at' => now(),
+        ]);
+
+        //act
+        $response = $this->actingAs($user)->post('api/accounts/' . $account->id . '/restore');
+
+        //assert
+        $response->assertStatus(200);
+        $this->assertNotSoftDeleted('accounts', ['id' => $account->id]);
     }
 }
--- tests/Feature/ProcessTransactionTest.php
+++ tests/Feature/ProcessTransactionTest.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Enums\TransactionStatus;
-use App\Enums\TransactionType;
+use App\Enums\TransactionStatusEnum;
+use App\Enums\TransactionTypeEnum;
 use App\Jobs\ProcessTransaction;
 use App\Models\Account;
 use App\Models\Transaction;
@@ -20,9 +20,9 @@ class ProcessTransactionTest extends TestCase
         ]);
         $transaction = Transaction::factory()->create([
             'account_id' => $account->id,
-            'type' => TransactionType::DEPOSIT->value,
+            'type' => TransactionTypeEnum::DEPOSIT->value,
             'amount' => 10000,
-            'status' => TransactionStatus::PENDING->value
+            'status' => TransactionStatusEnum::PENDING->value
         ]);
 
         // Act
@@ -31,5 +31,60 @@ class ProcessTransactionTest extends TestCase
         // Assert
         $this->assertEquals('completed', $transaction->fresh()->status);
         $this->assertEquals(10000, $account->fresh()->balance);
+    }
+
+    public function test_withdrawal_transaction()
+    {
+        // Arrange
+        $user = User::factory()->create();
+        $account = Account::factory()->create([
+            'user_id' => $user->id,
+            'balance' => 10000
+        ]);
+        $transaction = Transaction::factory()->create([
+            'account_id' => $account->id,
+            'type' => TransactionTypeEnum::WITHDRAWAL->value,
+            'amount' => 10000,
+            'status' => TransactionStatusEnum::PENDING->value
+        ]);
+
+        // Act
+        ProcessTransaction::dispatch($transaction);
+
+        // Assert
+        $this->assertEquals('completed', $transaction->fresh()->status);
+        $this->assertEquals(0, $account->fresh()->balance);
+    }
+
+    public function test_transfer_transaction()
+    {
+        // Arrange
+        $user = User::factory()->create();
+        $receiver = User::factory()->create();
+
+        $account_user = Account::factory()->create([
+            'user_id' => $user->id,
+            'balance' => 10000
+        ]);
+        $account_receiver = Account::factory()->create([
+            'user_id' => $receiver->id,
+            'balance' => 0
+        ]);
+
+        $transaction = Transaction::factory()->create([
+            'account_id' => $account_user->id,
+            'type' => TransactionTypeEnum::TRANSFER->value,
+            'destination_account_id' => $account_receiver->id,
+            'amount' => 10000,
+            'status' => TransactionStatusEnum::PENDING->value
+        ]);
+
+        // Act
+        ProcessTransaction::dispatch($transaction);
+
+        // Assert
+        $this->assertEquals('completed', $transaction->fresh()->status);
+        $this->assertEquals(0, $account_user->fresh()->balance);
+        $this->assertEquals(10000, $account_receiver->fresh()->balance);
     }
 }
